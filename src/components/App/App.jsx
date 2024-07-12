46af7174017740ddd17323ef40529501b8b46406
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-07-12 19:04:04 +0200

    add tailwind in the template

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-500 flex justify-center items-center h-screen">
+      <p className="text-6xl">Template React Redux Tailwind Vite</p>
+    </div>
+  );
+}
+
+export default App;

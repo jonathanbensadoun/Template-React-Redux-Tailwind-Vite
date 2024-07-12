cc3c8058b191ea479c1bb7d134015772b3c1faf8
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-07-12 19:51:09 +0200

    add typscript

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import NotFound from "../NotFound/NotFound";
+
 function App() {
   return (
     <Routes>

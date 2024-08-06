537362c5e8ffb0d6c18b3992121b4c4d5b5c2fe4
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-08-06 15:37:56 +0200

    template change color

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="bg-gray-500 flex justify-center items-center h-screen">
+    <div className="bg-purple-950 text-white flex justify-center items-center h-screen">
       <p className="text-6xl">Template React Redux Tailwind Vite</p>
     </div>
   );
